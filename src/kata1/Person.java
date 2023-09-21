46b3639a7e1d59244c43af77b77a418726c8b1fb
Tonmate <pedrom.rodriguezhernandez@gmail.com>
2023-09-21 19:14:12 +0100

    first commit

---
+++ src/kata1/Person.java
@@ -0,0 +1,40 @@
+package kata1;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private final  String name;
+    private final LocalDate birth;
+    private int petNumber;
+
+    public Person(String name, LocalDate birth, int petNumber) {
+        this.name = name;
+        this.birth = birth;
+        this.petNumber = petNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public int getPetNumber() {
+        return petNumber;
+    }
+
+    public void setPetNumber(int petNumber) {
+        this.petNumber = petNumber;
+    }
+
+    public int age(){
+        return (int) (LocalDate.now().toEpochDay() - birth.toEpochDay());
+    }
+
+    public void presentation(){
+        System.out.println("Hi, my name is "+getName()+"and i have got "+getPetNumber());
+    }
+}
